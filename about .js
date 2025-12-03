4ccec6ccf20919530767f5b425bf49d8cd2b240c
fredBode <fredbode84@gmail.com>
2025-12-03 22:42:39 +0300

    fixing errors

--- about .js
+++
@@ -1,4 +0,0 @@
-function showAddress() {
-    let address = "123 Main Street, Green Valley, New York,P.O BOX 1484 ,24KM Off the main road";
-    document.getElementById("address").textContent = address;
-}
